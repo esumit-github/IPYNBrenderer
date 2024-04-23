af1d90a1cf03bb08495374e3b8edfb7b31bf9061
Sumit <49120301+esumit-github@users.noreply.github.com>
2024-04-23 15:33:38 +0530

    tox updated

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START"
-echo [$(date)]: "Creating conda environment with python" 
-conda create --prefix ./env python=3.11 -y
-echo [$(date)]: "Active environment"
+echo [$(date)]: "Creating conda environment with python"
+conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "Activate env"
 source activate ./env
-echo [$(date)]: "Installing dev requirements"
+echo [$(date)]: "Intalling dev requirements"
 pip install -r requirements_dev.txt
 echo [$(date)]: "END"
